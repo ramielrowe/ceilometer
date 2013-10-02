b0589aec441a5438dbc351f390a71b43b601ab1f
Andrew Melton <ramielrowe@gmail.com>
2013-10-02 11:19:50 -0400

    Adding events to mongo storage engine
    partially implements bp stacktach-integration
    
    Change-Id: Ib73880195521a5cfec83399847c5374600ab2b0a

--- tests/storage/test_storage_scenarios.py
+++ tests/storage/test_storage_scenarios.py
@@ -2084,7 +2084,7 @@ class EventTest(EventTestBase):
              models.Event("2", "Zoo", now, [])]
         self.conn.record_events(m)
         for model in m:
-            self.assertTrue(model.id >= 0)
+            self.assertIsNotNone(model.id)
         self.assertNotEqual(m[0].id, m[1].id)
 
     def test_save_events_traits(self):
@@ -2105,7 +2105,7 @@ class EventTest(EventTestBase):
         self.conn.record_events(event_models)
         for model in event_models:
             for trait in model.traits:
-                self.assertTrue(trait.id >= 0)
+                self.assertIsNotNone(trait.id)
 
 
 class GetEventTest(EventTestBase):
